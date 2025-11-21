415cf909625a415f564474613aec89e99dc4fc00
hugorabelais <sio1@C106-09.220056s.local>
2025-11-21 17:11:57 +0100

    projet php

---
+++ barre_recherche.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>barre recherche</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-9">
+                <b>La bibliothèque de moulinsart est fermée au public jusqu'a nouvel ordre. Mais il vous est possible de reserver et retirer vos livres via notre service Biblio Drive! </b>
+                <div class="navbar navbar-light bg-light">
+                    <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="text" placeholder="recherche" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">recherche</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <img src="petit_chateau.jpg" alt="chateau" width="auto" height="auto" >
+            </div>
+        </div>
+    </div>    
+</body>
+</html>
